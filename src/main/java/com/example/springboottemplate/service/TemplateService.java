fdb6c1cefa7e10547d568797f55e093c8fbff05f
pengkk27 <1481128018@qq.com>
2022-06-05 14:37:50 +0800

    创建Service

---
+++ src/main/java/com/example/springboottemplate/service/TemplateService.java
@@ -0,0 +1,4 @@
+package com.example.springboottemplate.service;
+
+public interface TemplateService {
+}
---
+++ src/main/java/com/example/springboottemplate/service/impl/TemplateServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.springboottemplate.service.impl;
+
+import com.example.springboottemplate.service.TemplateService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemplateServiceImpl implements TemplateService {
+}
